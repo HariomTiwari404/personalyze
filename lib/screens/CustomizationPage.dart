b8c878258eaed9cda2c9368c38e2c77ddcb4ce83
roshan2708 <rs602543.com>
2025-02-25 22:30:53 +0530

    new changes

---
+++ lib/screens/CustomizationPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CustomizationPage extends StatefulWidget {
+  const CustomizationPage({super.key});
+
+  @override
+  State<CustomizationPage> createState() => _CustomizationPageState();
+}
+
+class _CustomizationPageState extends State<CustomizationPage> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold();
+  }
+}
